bd06a95d80c2caa7ac081f90e08bdee70ba3e886
Weblate <noreply@weblate.org>
2022-02-10 00:57:27 +0000

    Added translation using Weblate (Portuguese (Brazil))

---
+++ screenlock/translations/pt_BR.ts
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="pt_BR">
+<context>
+    <name>LockScreen</name>
+    <message>
+        <location filename="../lockscreen.ui" line="777"/>
+        <source>theShell</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="115"/>
+        <location filename="../lockscreen.ui" line="808"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="146"/>
+        <source>Unlock Session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="165"/>
+        <source>Switch User</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="191"/>
+        <source>Suspend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="214"/>
+        <source>Turn Off Screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="294"/>
+        <source>Please enter your password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="351"/>
+        <source>Caps Lock is on</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="517"/>
+        <source>Please enter your mouse password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="524"/>
+        <source>You entered the mouse password incorrectly too many times</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="713"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="729"/>
+        <source>Mouse Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="839"/>
+        <source>Switch Users</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="855"/>
+        <source>New Session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.ui" line="881"/>
+        <source>Who do you want to switch to?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.cpp" line="72"/>
+        <location filename="../lockscreen.cpp" line="73"/>
+        <location filename="../lockscreen.cpp" line="75"/>
+        <location filename="../lockscreen.cpp" line="76"/>
+        <source>Hello %1!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.cpp" line="84"/>
+        <source>Resume your session, continuing where you left off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.cpp" line="86"/>
+        <source>Enter your password and resume your session, continuing where you left off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.cpp" line="91"/>
+        <source>Unlock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.cpp" line="311"/>
+        <source>Mouse Password is disabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.cpp" line="378"/>
+        <source>Incorrect Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.cpp" line="379"/>
+        <source>The password you entered was incorrect. Please enter your password again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.cpp" line="547"/>
+        <source>%1 on X11 display %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.cpp" line="549"/>
+        <source>on %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.cpp" line="551"/>
+        <source>%1 on VT #%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lockscreen.cpp" line="553"/>
+        <source>Session</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TimerComplete</name>
+    <message>
+        <location filename="../timercomplete.ui" line="56"/>
+        <source>Stop Timer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timercomplete.ui" line="66"/>
+        <source>Stops the timer from making sounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timercomplete.ui" line="111"/>
+        <source>Timer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../timercomplete.ui" line="123"/>
+        <source>Time&apos;s up!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
